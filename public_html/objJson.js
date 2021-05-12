8df3ada7ba9f8f119af9b13b3f3906cf72d61d74
Hecz Klaudia <kicsihold21@gmail.com>
2021-05-12 09:46:31 +0200

    4. elkezdve

--- public_html/objJson.js
+++ public_html/objJson.js
@@ -1,17 +1,20 @@
 $(function () {
-    
-    for (var item in termekek) {
-        $("article").append(item+": "+termekek[item]+"<br>");
-        
-    }
-    
+  var szovegbolObjektum = JSON.parse(szovegJSON);
+
 });
 
+var szovegbolObjektum;
 
-var termekek= {
-    Fajta:"Kávé",
-    Nev:"Őrölt kávé",
-    Ar:900,
-    Mennyiseg:"500 g"
-    };
+var szovegJSON = '[{"Fajta":"Kávé","Nev":"Őrölt kávé","Ar":900,"Mennyiseg":"500 g"},{"Fajta":"Kopi Luwak","Nev": "frissen pörkölt single origin arabica kávé különlegesség Jáva szigetéről", "Ar": 5000, "Mennyiseg": "500 g"}]';
 
+$("article").append("<table>");
+$("article table").append("");
+
+function kiir() {
+  for (var i = 0; i < szovegbolObjektum.length; i++) {
+    $("article table").append("<tr>");
+  }
+  for (var item in szovegbolObjektum[i]) {
+    
+  }
+}
