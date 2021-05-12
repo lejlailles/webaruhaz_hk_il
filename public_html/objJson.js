4a150368d3cba50bcd93e2134e3b16852bcded2d
lejlailles <il970322@gmail.com>
2021-05-12 09:38:34 +0200

    3. feladat

--- public_html/objJson.js
+++ public_html/objJson.js
@@ -1,17 +1,7 @@
 $(function () {
-    
-    for (var item in termekek) {
-        $("article").append(item+": "+termekek[item]+"<br>");
+        var szovegbolObjektum=JSON.parse(szovegJSON);
         
-    }
     
 });
 
-
-var termekek= {
-    Fajta:"Kávé",
-    Nev:"Őrölt kávé",
-    Ar:900,
-    Mennyiseg:"500 g"
-    };
-
+var szovegJSON='[{"Fajta":"Kávé","Nev":"Őrölt kávé","Ar":900,"Mennyiseg":"500 g"},{"Fajta":"Kopi Luwak", "Nev":"frissen pörkölt single origin arabica kávé különlegesség Jáva szigetéről","Ar":5000,"Mennyiseg":"500 g"}]';
